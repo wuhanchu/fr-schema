dbef6b07d4a276b06a1393a8968f2ab58cef34e1
whcsdtc <whcwuhanchu@gmail.com>
2019-08-25 11:25:28 +0800

    commit

--- src/moment.js
+++ src/moment.js
@@ -1,7 +1,7 @@
 import moment from "moment"
 
-export const DATE_FORMAT = "YYYY年MM月DD日"
-export const DATE_TIME_FORMAT = "YYYY年MM月DD日 HH:mm:ss"
+export const DATE_FORMAT = "YYYY-MM-DD"
+export const DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss"
 export const TME_FORMAT = "HH:mm:ss"
 
 /**
--- src/momnet.js
+++
@@ -1,3 +0,0 @@
-export const DATE_FORMAT = "YYYY年MM月DD日"
-export const DATE_TIME_FORMAT = "YYYY年MM月DD日 HH:mm:ss"
-export const TME_FORMAT = "HH:mm:ss"
