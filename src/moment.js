eca6190f6adfcec4b613ca82c3c1c551ba7e8161
wuhanchu <whcwuhanchu@gmail.com>
2019-10-05 21:53:12 +0800

    delete lib

--- src/moment.js
+++ src/moment.js
@@ -1,5 +1,7 @@
 import moment from "moment"
 
+export const YEAR_FORMAT = "YYYY"
+export const MONTH_FORMAT = "YYYY-MM"
 export const DATE_FORMAT = "YYYY-MM-DD"
 export const DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss"
 export const TME_FORMAT = "HH:mm:ss"
