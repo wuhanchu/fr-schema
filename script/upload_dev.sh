2c23d2d5275b9248d0bcc2cbf13b0f9ffe93dbff
wuhanchu <whcwuhanchu@gmail.com>
2019-12-17 10:02:35 +0800

    update upload_dev.sh

--- script/upload_dev.sh
+++ script/upload_dev.sh
@@ -6,4 +6,4 @@ scp -P 15022 ./dist.zip root@39.105.193.154:/data/tmp
 ssh -p 15022 root@39.105.193.154 "cd /data/tmp; unzip -o dist; \cp -fr ./dist/* /data/z_markgo_web;"
 rm ./dist.zip
 git checkout dist
-git clean -f dist
+git clean -fd dist
