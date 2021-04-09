63e406b58615b1b980c8f2346f5aeea8626d4fae
xujinhao <2973703020@qq.com>
2021-04-09 15:18:47 +0800

    提交

--- src/utils/minio.js
+++ src/utils/minio.js
@@ -9,7 +9,7 @@ console.log(document.location.protocol)
 if (document.domain === "server.aiknown.cn") {
     endPoint = "server.aiknown.cn"
     port = 31321
-    isHttps = true
+    isHttps = false
 }
 var minioClient = new Minio.Client({
     endPoint: endPoint,
