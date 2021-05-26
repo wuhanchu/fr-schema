f5af057d6c681ba27aeae8a50724a60084199824
xujinhao <2973703020@qq.com>
2021-05-26 09:29:21 +0800

    提交

--- src/pages/domain/List.jsx
+++ src/pages/domain/List.jsx
@@ -12,64 +12,11 @@ import InfoModal from "@/outter/fr-schema-antd-utils/src/components/Page/InfoMod
 import frSchema from "@/outter/fr-schema/src"
 import { listToDict } from "@/outter/fr-schema/src/dict"
 import { dataConvert } from "@/pages/authority/department/DataList"
+import UserTransfer from "./component/UserTransfer"
 import departmentService from "@/pages/authority/department/service"
 import clone from "clone"
 
 const { schemaFieldType } = frSchema
-
-function searchTree(tree, id) {
-    let res = findNode(tree, id)
-
-    //边界处理，输入的id不存在相对应的节点时
-    if (res == undefined) {
-        return "在该树的中没有相对应的id的节点"
-    }
-
-    res.path.unshift(tree.name)
-    let path = res.path.join("/")
-    let node = res.node
-    let leaves = findLeaves(node)
-    return {
-        path,
-        leaves,
-    }
-}
-
-// 深度遍历查找目标节点及缓存相关路径
-function findNode(tree, id) {
-    if (tree.key == id) {
-        return {
-            path: [],
-            node: tree,
-        }
-    }
-
-    // console.log(tree)
-
-    let res
-    for (let i = 0; i < tree.children.length; i++) {
-        res = findNode(tree.children[i], id)
-        if (res != undefined) {
-            res.path.unshift(tree.children[i].name)
-            return res
-        }
-    }
-    return undefined
-}
-
-// 递归获取叶子节点
-function findLeaves(node) {
-    if (node.children.length == 0) {
-        return [node.key]
-    }
-    let leaves = []
-    let res
-    for (let i = 0; i < node.children.length; i++) {
-        res = findLeaves(node.children[i])
-        leaves = res.concat(leaves)
-    }
-    return leaves
-}
 
 @connect(({ global }) => ({
     dict: global.dict,
@@ -96,33 +43,8 @@ class List extends ListPage {
         this.schema.talk_service_id.dict = listToDict(aiService.list)
         const data = await this.service.getUserAuthUser()
 
-        const response = await departmentService.get({
-            limit: 1000,
-        })
-
-        let list = response.list.map((item) => {
-            return { ...item, children: [] }
-        })
-
-        let departmentConvert = dataConvert(clone({ list: list }))
-
-        let nameDict = {}
-        response.list.map((item, index) => {
-            console.log(item.key)
-            nameDict[item.key] = {
-                ...searchTree(
-                    { children: departmentConvert.list, key: "qw" },
-                    item.key
-                ),
-                key: item.key,
-            }
-        })
-        console.log(nameDict)
-        const departmentDict = listToDict(response.list, null, "key", "name")
         this.setState({
             userList: data,
-            nameDict,
-            departmentDict,
         })
 
         super.componentDidMount()
@@ -226,66 +148,39 @@ class List extends ListPage {
         )
     }
 
-    getName = (data) => {
-        let name = ""
-        data.department_key &&
-            data.department_key[0] &&
-            this.state.departmentDict &&
-            data.department_key.map((item, index) => {
-                if (index !== data.department_key.length - 1)
-                    name = name + this.state.nameDict[item].path.substr(1) + "-"
-                else {
-                    name = name + this.state.nameDict[item].path.substr(1)
-                }
-            })
-        return name
-    }
-
     renderAssignModal() {
-        const dataSource =
-            this.state.userList &&
-            this.state.userList.list.map((item) => {
-                let name = this.getName(item)
-                if (name) {
-                    name = "(" + name + ")"
-                }
-                return {
-                    key: item.id,
-                    ...item,
-                    name: item.name + name,
-                }
-            })
         const { visibleAssign } = this.state
-        const schemas = {
-            users_id: {
-                title: "人员",
-                type: schemaFieldType.Transfer,
-                itemProps: {
-                    labelCol: {
-                        span: 3,
-                    },
-                },
-                props: {
-                    dataSource,
-                    style: { width: 850 },
-                    listStyle: {
-                        width: 400,
-                        height: 400,
-                    },
-                },
-            },
-        }
-        //  return
-        console.log(this.state.teamHaveUser)
         return (
+            // visibleAssign && (
+            //     <InfoModal
+            //         title="人员分配"
+            //         onCancel={() => {
+            //             this.setState({ visibleAssign: false })
+            //         }}
+            //         action="add"
+            //         width="1100px"
+            //         handleAdd={(...props) => {
+            //             this.handleUpdates(
+            //                 ...props.concat([this.schema, "put"]),
+            //                 null,
+            //                 "put"
+            //             )
+            //             this.setState({ visibleAssign: false })
+            //         }}
+            //         visible
+            //         values={{ users_id: this.state.teamHaveUser }}
+            //         schema={schemas}
+            //     />
+            // )
             visibleAssign && (
-                <InfoModal
+                <UserTransfer
                     title="人员分配"
                     onCancel={() => {
                         this.setState({ visibleAssign: false })
                     }}
                     action="add"
                     width="1100px"
+                    userList={this.state.userList}
                     handleAdd={(...props) => {
                         this.handleUpdates(
                             ...props.concat([this.schema, "put"]),
@@ -297,6 +192,7 @@ class List extends ListPage {
                     visible
                     values={{ users_id: this.state.teamHaveUser }}
                     schema={schemas}
+                    service={this.service}
                 />
             )
         )
--- src/pages/domain/component/UserTransfer.js
+++ src/pages/domain/component/UserTransfer.js
@@ -1,126 +1,361 @@
-import React, { useState } from "react"
-import { Transfer, Tree, Modal } from "antd"
+import React, { PureComponent } from "react"
+import { Form } from "@ant-design/compatible"
+import "@ant-design/compatible/assets/index.css"
+import { message, Modal, Skeleton, Spin } from "antd"
+import InfoForm from "@/outter/fr-schema-antd-utils/src/components/Page/InfoForm"
+import departmentService from "@/pages/authority/department/service"
+import { dataConvert } from "@/pages/authority/department/DataList"
+import clone from "clone"
+import frSchema from "@/outter/fr-schema/src"
+import { listToDict } from "@/outter/fr-schema/src/dict"
 
-// Customize Table Transfer
-const isChecked = (selectedKeys, eventKey) =>
-    selectedKeys.indexOf(eventKey) !== -1
+const { actions, getPrimaryKey, schemaFieldType } = frSchema
 
-const generateTree = (treeNodes = [], checkedKeys = []) =>
-    treeNodes.map(({ children, ...props }) => ({
-        ...props,
-        disabled: checkedKeys.includes(props.key),
-        children: generateTree(children, checkedKeys),
-    }))
+const confirm = Modal.confirm
 
-const TreeTransfer = ({ dataSource, targetKeys, ...restProps }) => {
-    const transferDataSource = []
-    function flatten(list = []) {
-        list.forEach((item) => {
-            transferDataSource.push(item)
-            flatten(item.children)
+function searchTree(tree, id) {
+    let res = findNode(tree, id)
+
+    //边界处理，输入的id不存在相对应的节点时
+    if (res == undefined) {
+        return "在该树的中没有相对应的id的节点"
+    }
+
+    res.path.unshift(tree.name)
+    let path = res.path.join("/")
+    let node = res.node
+    let leaves = findLeaves(node)
+    return {
+        path,
+        leaves,
+    }
+}
+
+// 深度遍历查找目标节点及缓存相关路径
+function findNode(tree, id) {
+    if (tree.key == id) {
+        return {
+            path: [],
+            node: tree,
+        }
+    }
+
+    // console.log(tree)
+
+    let res
+    for (let i = 0; i < tree.children.length; i++) {
+        res = findNode(tree.children[i], id)
+        if (res != undefined) {
+            res.path.unshift(tree.children[i].name)
+            return res
+        }
+    }
+    return undefined
+}
+
+// 递归获取叶子节点
+function findLeaves(node) {
+    if (node.children.length == 0) {
+        return [node.key]
+    }
+    let leaves = []
+    let res
+    for (let i = 0; i < node.children.length; i++) {
+        res = findLeaves(node.children[i])
+        leaves = res.concat(leaves)
+    }
+    return leaves
+}
+
+export class UserTransfer extends PureComponent {
+    formRef = React.createRef()
+
+    state = {
+        loadingFetch: true,
+        loadingSubmit: false,
+    }
+
+    constructor(props) {
+        super(props)
+        const { addArgs, visible, onRef, values, service } = props
+        this.service = service
+        this.state.visible = visible
+        this.state.values = values
+        onRef && onRef(this)
+    }
+
+    show = (values) => {
+        const { addArgs } = this.props
+
+        this.setState({
+            visible: true,
+            values: { ...addArgs, ...values },
         })
     }
-    flatten(dataSource)
-    const [selectedKeys, setSelectedKeys] = useState([])
-    const onCheck = (checkedKeysValue, info, onItemSelectAll, checkedKeys) => {
-        onItemSelectAll(checkedKeysValue, true)
+
+    hide = () => {
+        this.setState({
+            visible: false,
+            values: null,
+        })
     }
 
-    const onSelect = (selectedKeysValue, info) => {
-        console.log("onSelect", info)
-    }
-    return (
-        <Transfer
-            {...restProps}
-            showSearch
-            targetKeys={targetKeys}
-            dataSource={transferDataSource}
-            className="tree-transfer"
-            render={(item) => item.title}
-            showSelectAll={false}
-        >
-            {({ direction, onItemSelect, selectedKeys, onItemSelectAll }) => {
-                if (direction === "left") {
-                    const checkedKeys = [...selectedKeys, ...targetKeys]
-                    return (
-                        <Tree
-                            blockNode
-                            defaultExpandAll
-                            checkable
-                            // checkedKeys={checkedKeys}
-                            onSelect={onSelect}
-                            onCheck={(checkedKeysValue, info) => {
-                                onCheck(
-                                    checkedKeysValue,
-                                    info,
-                                    onItemSelectAll,
-                                    checkedKeys
-                                )
-                            }}
-                            // treeData={treeData}
-                            treeData={generateTree(dataSource, targetKeys)}
-                            defaultCheckedKeys={[
-                                "0-0",
-                                "0-1-0",
-                                "0-1-1-0",
-                                "0-1-1-1",
-                            ]}
-                        />
-                    )
+    async componentDidMount() {
+        const response = await departmentService.get({
+            limit: 1000,
+        })
+
+        let list = response.list.map((item) => {
+            return { ...item, children: [] }
+        })
+
+        let departmentConvert = dataConvert(clone({ list: list }))
+
+        let nameDict = {}
+        response.list.map((item, index) => {
+            console.log(item.key)
+            nameDict[item.key] = {
+                ...searchTree(
+                    { children: departmentConvert.list, key: "qw" },
+                    item.key
+                ),
+                key: item.key,
+            }
+        })
+        console.log(nameDict)
+        const departmentDict = listToDict(response.list, null, "key", "name")
+        this.setState({
+            nameDict,
+            departmentDict,
+        })
+
+        const dataSource =
+            this.props.userList &&
+            this.props.userList.list.map((item) => {
+                let name = this.getName(item)
+                if (name) {
+                    name = "(" + name + ")"
                 }
-            }}
-        </Transfer>
-    )
-}
-
-const treeData = [
-    { key: "0-0", title: "0-0" },
-    {
-        key: "0-1",
-        title: "0-1",
-        children: [
-            { key: "0-1-0", title: "0-1-0" },
-            {
-                key: "0-1-1",
-                title: "0-1-1",
-                children: [
-                    { key: "0-1-1-0", title: "0-1-1-0" },
-                    { key: "0-1-1-1", title: "0-1-1-1" },
-                ],
+                return {
+                    key: item.id,
+                    ...item,
+                    name: item.name + name,
+                }
+            })
+        const { visibleAssign } = this.state
+        const schemas = {
+            users_id: {
+                title: "人员",
+                type: schemaFieldType.Transfer,
+                itemProps: {
+                    labelCol: {
+                        span: 3,
+                    },
+                },
+                props: {
+                    dataSource,
+                    style: { width: 850 },
+                    listStyle: {
+                        width: 400,
+                        height: 400,
+                    },
+                },
             },
-        ],
-    },
-    { key: "0-2", title: "0-3" },
-]
+        }
+        this.schema = schemas
 
-const MYTransfer = () => {
-    const [targetKeys, setTargetKeys] = useState([])
-    const onChange = (keys) => {
-        setTargetKeys(keys)
+        this.setState({ loadingFetch: false })
+        const { componentDidMount } = this.props
+        componentDidMount && componentDidMount()
     }
-    return (
-        <TreeTransfer
-            listStyle={{
-                width: 350,
-                height: 300,
-            }}
-            dataSource={treeData}
-            targetKeys={targetKeys}
-            onChange={onChange}
-        />
-    )
-}
 
-class UserTransfer extends React.Component {
+    getName = (data) => {
+        let name = ""
+        data.department_key &&
+            data.department_key[0] &&
+            this.state.departmentDict &&
+            data.department_key.map((item, index) => {
+                if (index !== data.department_key.length - 1)
+                    name = name + this.state.nameDict[item].path.substr(1) + "-"
+                else {
+                    name = name + this.state.nameDict[item].path.substr(1)
+                }
+            })
+        return name
+    }
+
+    /**
+     * 确认保存
+     */
+    handleSave = () => {
+        const { handleUpdate, handleAdd, action, addArgs } = this.props
+        const { values } = this.state
+
+        this.setState({ loadingSubmit: true })
+
+        this.formRef.current
+            .validateFields()
+            .then(async (fieldsValue) => {
+                let param = addArgs ? { ...addArgs } : {}
+                const idKey = getPrimaryKey(this.schema)
+
+                // set the id value
+                if (values) {
+                    const idValue = values[idKey || "id"]
+                    idValue && (param[idKey] = idValue)
+                }
+
+                Object.keys(fieldsValue).forEach((key) => {
+                    param[key] =
+                        fieldsValue[key] instanceof Array &&
+                        fieldsValue[key][0] instanceof String
+                            ? fieldsValue[key].join(",")
+                            : fieldsValue[key]
+                })
+
+                if (this.props.convertParam) {
+                    param = this.props.convertParam(param)
+                }
+
+                if (action === actions.edit) {
+                    await handleUpdate(param, this.schema)
+                } else {
+                    await handleAdd(param, this.schema)
+                }
+            })
+            .catch((err) => {
+                console.log("err", err)
+            })
+            .finally((_) => {
+                this.setState({ loadingSubmit: false })
+            })
+    }
+
+    /**
+     * 渲染表单
+     * @returns {*}
+     */
+    renderForm() {
+        const { values } = this.state
+        console.log(this.state)
+        console.log(this.props)
+        console.log(values)
+        // 只传入InForm所需参数,避免console报错
+        const {
+            renderForm,
+            handleVisibleModal,
+            handleUpdate,
+            handleAdd,
+            visible,
+            addArgs,
+            ...otherProps
+        } = this.props
+        return renderForm && typeof renderForm === "function" ? (
+            renderForm({
+                ...this.props,
+                values,
+                schema: this.schema,
+            })
+        ) : (
+            <InfoForm
+                {...otherProps}
+                values={values}
+                schema={this.schema}
+                form={this.formRef}
+            />
+        )
+    }
+
+    /**
+     * 弹出框关闭前调用
+     */
+    beforeFormClose = () => {
+        const { values } = this.state
+        const { action } = this.props
+        const fieldsValue = this.formRef.current.getFieldsValue()
+        const flag = Object.keys(fieldsValue).some((key) => {
+            let result =
+                fieldsValue[key] && (!values || values[key] != fieldsValue[key])
+
+            // moment 比较
+            if (fieldsValue[key] && fieldsValue[key].isSame && values) {
+                result = !fieldsValue[key].isSame(values[key])
+            }
+
+            if (result) {
+                console.log(`字段${key}被修改为${values}`)
+            }
+            return result
+        })
+
+        if (flag && action !== actions.show) {
+            confirm({
+                title: "提示",
+                content: "数据已做修改，确认关闭对话框？",
+                okText: "关闭",
+                cancelText: "取消",
+                onOk: () => {
+                    this.closeModel()
+                },
+                onCancel: () => {
+                    return
+                },
+            })
+            return false
+        } else {
+            this.closeModel()
+        }
+    }
+
+    /**
+     * 关闭弹出框
+     */
+    closeModel = () => {
+        const { handleVisibleModal } = this.props
+        this.setState({
+            visible: false,
+        })
+        handleVisibleModal && handleVisibleModal()
+    }
+
     render() {
+        const { loadingSubmit } = this.state
+
+        const { title, action, ...otherProps } = this.props
+        const { visible, values } = this.state
+
+        if (!visible) {
+            return null
+        }
+
+        // 查看模式 不需要显示 按钮
+        if (action == actions.show) {
+            otherProps.footer = null
+        }
+
         return (
             <Modal
-                width={900}
+                width={700}
+                destroyOnClose
+                title={title || "" + "信息"}
                 visible={true}
-                onCancel={this.props.onCancel}
-                title={"人员分配"}
+                onOk={this.handleSave}
+                okButtonProps={{
+                    loading: this.props.loadingSubmit || loadingSubmit,
+                }}
+                {...otherProps}
+                onCancel={() => {
+                    if (this.beforeFormClose() === false) {
+                        return false
+                    }
+                    otherProps.onCancel && otherProps.onCancel()
+                }}
             >
-                <MYTransfer />
+                {this.state.loadingFetch ? (
+                    <Skeleton />
+                ) : (
+                    <Spin spinning={loadingSubmit}>{this.renderForm()}</Spin>
+                )}
             </Modal>
         )
     }
