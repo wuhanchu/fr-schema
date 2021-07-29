811f678e6a780cb250d1e1f79e3e92473e7234f8
xujinhao <2973703020@qq.com>
2021-07-29 14:13:14 +0800

    流程图意图

--- src/schemas/flow/node.js
+++ src/schemas/flow/node.js
@@ -1,31 +1,119 @@
 import { createApi } from "@/outter/fr-schema/src/service"
 
-
 import { schemaFieldType } from "@/outter/fr-schema/src/schema"
 
-
 let service = createApi("flow_node")
-service.get = ()=>{
-    return {list:[{id: "12345-1234-123-12-123", name: '再见!', type: 2, x: 410, y: 550},{id: "12345-1234-123-12-1", name: '开始', type: 2, x:410, y: 250},{id: "12345-1234-123-12-12", name: '对不起，打错了',x: 410, y: 400, type: 2, intentions: [{
-        id: '12345-1234-123-12-112', node_id: '12345-1234-123-12-1', to_node_id: '12345-1234-123-12-12', name: '任意',type: 1
-    }, {
-        id: '12345-1234-123-12-113', node_id: '12345-1234-123-12-12', to_node_id: '12345-1234-123-12-123', name: '测试',type: 1
-    }]}]}
+service.get = () => {
+    return {
+        list: [
+            {
+                id: "12345-1234-123-12-123",
+                name: "再见!",
+                type: 2,
+                x: 410,
+                y: 550,
+            },
+            {
+                id: "12345-1234-123-12-1",
+                name: "开始",
+                type: 2,
+                x: 410,
+                y: 250,
+            },
+            {
+                id: "12345-1234-123-12-12",
+                name: "对不起，打错了",
+                x: 410,
+                y: 400,
+                type: 2,
+                intentions: [
+                    {
+                        answer_type: 2,
+                        id: "12345-1234-123-12-112",
+                        source: "12345-1234-123-12-1",
+                        target: "12345-1234-123-12-12",
+                        node_id: "12345-1234-123-12-1",
+                        to_node_id: "12345-1234-123-12-12",
+                        type: 1,
+                    },
+                    {
+                        answer_type: 1,
+                        id: "12345-1234-123-12-113",
+                        source: "12345-1234-123-12-123",
+                        target: "12345-1234-123-12-12",
+                        node_id: "12345-1234-123-12-12",
+                        to_node_id: "12345-1234-123-12-123",
+                        type: 1,
+                    },
+                ],
+            },
+        ],
+    }
 }
-service.getDetail = ()=>{
-    return [{id: "12345-1234-123-12-123", name: '肯定', type: 3},{id: "12345-1234-123-12-1", name: '肯定', type: 2},{id: "12345-1234-123-12-12", name: '否定', type: 1, intentions: [{
-        id: '12345-1234-123-12-112', node_id: '12345-1234-123-12-1', to_node_id: '12345-1234-123-12-12', name: '测试',type: 1
-    }, {
-        id: '12345-1234-123-12-113', node_id: '12345-1234-123-12-123', to_node_id: '12345-1234-123-12-12', name: '测试',type: 1
-    }]}]
+service.getDetail = () => {
+    return [
+        { id: "12345-1234-123-12-123", name: "肯定", type: 3 },
+        { id: "12345-1234-123-12-1", name: "肯定", type: 2 },
+        {
+            id: "12345-1234-123-12-12",
+            name: "否定",
+            type: 1,
+            intentions: [
+                {
+                    answer_type: 2,
+                    id: "12345-1234-123-12-112",
+                    node_id: "12345-1234-123-12-1",
+                    source: "12345-1234-123-12-1",
+                    target: "12345-1234-123-12-12",
+                    to_node_id: "12345-1234-123-12-12",
+                    type: 1,
+                },
+                {
+                    answer_type: 1,
+                    id: "12345-1234-123-12-113",
+                    source: "12345-1234-123-12-123",
+                    target: "12345-1234-123-12-12",
+                    node_id: "12345-1234-123-12-123",
+                    to_node_id: "12345-1234-123-12-12",
+                    type: 1,
+                },
+            ],
+        },
+    ]
 }
-service.post = ()=>{
+service.post = () => {
     console.log("post")
-    return {list: [{id: "12345-1234-123-12-123", name: '肯定', type: 3},{id: "12345-1234-123-12-1", name: '肯定', type: 2},{id: "12345-1234-123-12-12", name: '否定', type: 1, intentions: [{
-        id: '12345-1234-123-12-112', node_id: '12345-1234-123-12-1', to_node_id: '12345-1234-123-12-12', name: '测试',type: 1
-    }, {
-        id: '12345-1234-123-12-113', node_id: '12345-1234-123-12-123', to_node_id: '12345-1234-123-12-12', name: '测试',type: 1
-    }]}]}
+    return {
+        list: [
+            { id: "12345-1234-123-12-123", name: "肯定", type: 3 },
+            { id: "12345-1234-123-12-1", name: "肯定", type: 2 },
+            {
+                id: "12345-1234-123-12-12",
+                name: "否定",
+                type: 1,
+                intentions: [
+                    {
+                        answer_type: 2,
+                        id: "12345-1234-123-12-112",
+                        source: "12345-1234-123-12-1",
+                        target: "12345-1234-123-12-12",
+                        node_id: "12345-1234-123-12-1",
+                        to_node_id: "12345-1234-123-12-12",
+                        type: 1,
+                    },
+                    {
+                        answer_type: 1,
+                        id: "12345-1234-123-12-113",
+                        source: "12345-1234-123-12-123",
+                        target: "12345-1234-123-12-12",
+                        node_id: "12345-1234-123-12-123",
+                        to_node_id: "12345-1234-123-12-12",
+                        type: 1,
+                    },
+                ],
+            },
+        ],
+    }
 }
 export default {
     service,
@@ -37,30 +125,30 @@ export default {
                 normal: {
                     value: 1,
                     remark: "普通话术",
-                    default: true
+                    default: true,
                 },
                 word: {
                     value: 2,
-                    remark: "任意回答话术"
+                    remark: "任意回答话术",
                 },
                 end: {
                     value: 3,
-                    remark: " 结束语"
+                    remark: " 结束语",
                 },
                 start: {
                     value: 0,
-                    remark: " 开始话术"
-                }
+                    remark: " 开始话术",
+                },
             },
             props: {
-                style: { width: 180 }
-            }
+                style: { width: 180 },
+            },
         },
         name: {
             title: "名称",
             props: {
-                style: { width: 180 }
-            }
+                style: { width: 180 },
+            },
         },
         desc: {
             title: "话术内容",
@@ -68,8 +156,8 @@ export default {
             props: {
                 style: { width: 150, height: 150 },
                 autoSize: true,
-                value: "123"
-            }
-        }
-    }
+                value: "123",
+            },
+        },
+    },
 }
